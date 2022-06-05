d32eb7f4ca13c3402ffe0c4d4292f2117dd4bf60
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-06-05 14:23:02 +0100

    Made a response grid

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 let winningWord = "australia";
-const height = 6;
-const width = 5;
 
-function initialiseBoard() {
-  for (let i = 0; i < height; i++) {
-    let row = document.createElement("div");
-    row.className = "letter-row";
+const container = document.getElementById("container");
 
-    for (let j = 0; j < width; j++) {
-      let box = document.createElement("div");
-      box.id = i.toString() + "-" + j.toString();
-      box.classList.add("box");
-      box.innerText = "";
-
-      document.getElementById("game-board").appendChild(box);
-    }
+function makeRows(rows, cols) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
   }
 }
 
-initialiseBoard();
+makeRows(6, winningWord.length);
